31a56b0363bdb2417243bb23f39e5455affc92c1
jacusw22 <jacusw22@gmail.com>
2020-05-20 21:58:44 +0100

    Commit message

--- startup.sh
+++ startup.sh
@@ -1,38 +1,51 @@
-#!bin/bash
+!bin/bash
 
 # We use apt because its never command which was invented in Ubuntu 16
 # If you use Mint change it to apt-get
 
 # update repositories informations
-sudo apt update
+#sudo apt update
 
 # upgrade all packeges installed on your system
-sudo apt upgrade
+#sudo apt upgrade
 
 # Midnight commander
-sudo apt install mc
+#sudo apt install mc -y
 
 # FileZilla (FTP client)
-sudo apt install filezilla
+#sudo apt install filezilla -y
 
 # VLC video player
-sudo apt install vlc
+#sudo apt install vlc -y
 
 
 # Add repository with KeePassXC and update
-sudo add-apt-repository ppa:phoerious/keepassxc
-sudo apt update
+#sudo add-apt-repository ppa:phoerious/keepassxc
+#sudo apt update
 
-sudo apt install keepassxc
+#sudo apt install keepassxc -y
 
-#download and install zoom
+#download snap and install zoom
 
-wget https://zoom.us/client/latest/zoom_amd64.deb
+#sudo apt install snapd
 
-dpkg -i zoom_amd64.deb > /dev/null #sending error to null
+#remove unecessary files
+#sudo apt autoremove -y 
 
-apt -f install
+#sudo snap install zoom-client
+#install visual studio code
+#sudo snap install code --classic
 
-apt install ./zoom_amd64.deb
+#sudo add-apt-repository ppa:transmissionbt/ppa
+#sudo apt-get update -y
 
-rm zoom_amd64.deb 
+#install telegram client
+#sudo snap install telegram-desktop
+
+#install calibre ebook reader
+#sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+#install SkypeOnLinux
+#sudo snap install skype --classic
+
+sudo apt-get update & sudo apt-get upgrade
