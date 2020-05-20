f0844221d0023ad3950f19480e8290069215031d
jacusw22 <jacusw22@gmail.com>
2020-05-20 22:12:33 +0100

    commiti message

--- startup.sh
+++ startup.sh
@@ -4,48 +4,49 @@
 # If you use Mint change it to apt-get
 
 # update repositories informations
-#sudo apt update
+sudo apt update
 
 # upgrade all packeges installed on your system
-#sudo apt upgrade
+sudo apt upgrade
 
 # Midnight commander
-#sudo apt install mc -y
+sudo apt install mc -y
 
 # FileZilla (FTP client)
-#sudo apt install filezilla -y
+sudo apt install filezilla -y
 
 # VLC video player
-#sudo apt install vlc -y
+sudo apt install vlc -y
 
 
 # Add repository with KeePassXC and update
-#sudo add-apt-repository ppa:phoerious/keepassxc
-#sudo apt update
+sudo add-apt-repository ppa:phoerious/keepassxc
+sudo apt update
 
-#sudo apt install keepassxc -y
+sudo apt install keepassxc -y
 
 #download snap and install zoom
 
-#sudo apt install snapd
+sudo apt install snapd
 
 #remove unecessary files
-#sudo apt autoremove -y 
+sudo apt autoremove -y 
 
-#sudo snap install zoom-client
+sudo snap install zoom-client
+
 #install visual studio code
-#sudo snap install code --classic
+sudo snap install code --classic
 
 #sudo add-apt-repository ppa:transmissionbt/ppa
-#sudo apt-get update -y
+sudo apt-get update -y
 
 #install telegram client
-#sudo snap install telegram-desktop
+sudo snap install telegram-desktop
 
 #install calibre ebook reader
-#sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 
 #install SkypeOnLinux
-#sudo snap install skype --classic
+sudo snap install skype --classic
 
 sudo apt-get update & sudo apt-get upgrade
